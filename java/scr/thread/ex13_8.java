87a4bc2243bef790530691809774e702ad609ecc
suji5262 <suji5262@gmail.com>
2022-12-09 18:51:00 +0900

    22.12.09 file add

--- java/scr/thread/ex13_8.java
+++ java/scr/thread/ex13_8.java
@@ -8,23 +8,26 @@ class ex13_8 {
         th2.start();
 
         try {
-            th1.sleep(2000);
+//            th1.sleep(2000);  // 이렇게 쓰면 오해가능 - main 쓰레드가 잠이든다(코드를 실행하는건 메인)
+            Thread.sleep(2000); // 이렇게 써야 보다 편함
         } catch (InterruptedException e) {}
+
+        System.out.print("<< main 종료 >>");
     } // main
-}
+} // 쓰레드가 3개있음 (main, th1, th2)
 
 class ThreadEx8_1 extends Thread {
     public void run() {
         for (int i = 0; i < 300; i++)
-            System.out.println("_");
-        System.out.println("<< th1 종료 >>");
+            System.out.print("_");
+        System.out.print("<< th1 종료 >>");
     } // run()
 }
 
 class ThreadEx8_2 extends Thread {
     public void run() {
         for (int i = 0; i < 300; i++)
-            System.out.println("|");
-        System.out.println("<< th2 종료 >>");
+            System.out.print("|");
+        System.out.print("<< th2 종료 >>");
     } // run()
 }
