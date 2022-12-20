418db32b282b865f727a972d3ba7f9edefd96b34
suji5262 <suji5262@gmail.com>
2022-12-20 13:35:56 +0900

    22.12.20 variable 공부파일 add

---
+++ java/scr/variable/Ex2_1.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Ex2_1 {
+    public static void main(String[] args) {
+        int x = 4, y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        int age = 14;
+        int year = 0;
+
+        System.out.println(year);
+        System.out.println(age);
+        year = age + 2000;
+        age = age +1;
+
+        System.out.println(year);
+        System.out.println(age);
+
+    }
+}
